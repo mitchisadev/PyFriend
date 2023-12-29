b48c1465c27a85ff81a2360a4cf1e8c1ea9e3387
mitchisadev <115275192+mitchisadev@users.noreply.github.com>
2023-12-29 16:57:49 -0600

    hotfix for rasberry pi
    because for some OGD UNKOWN REASON my raspberry pi WILL NOT DOWNLOAD PYTHON 3.10, which is the update where they added match cases.

--- main.py
+++ main.py
@@ -8,14 +8,13 @@ def main():
 
     result = speechHandler.record()
     
-    match result:
         
-        case 'happy':
-            displayHandler.show_full_screen_image(result, displayHandler.happy)
-        case 'sad':
-            displayHandler.show_full_screen_image(result, displayHandler.sad)
-        case 'angry':
-            displayHandler.show_full_screen_image(result, displayHandler.angry)
+    if result == 'happy':
+        displayHandler.show_full_screen_image(result, displayHandler.happy)
+    elif result == 'sad':
+        displayHandler.show_full_screen_image(result, displayHandler.sad)
+    elif result == 'angry':
+        displayHandler.show_full_screen_image(result, displayHandler.angry)
     
 
 if __name__ == '__main__':
